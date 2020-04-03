c0d5eb8f8cde06c94d334d4a3a3dfc887e628218
Jon Butler <Jwb2472@gmail.com>
2020-04-03 00:23:15 -0400

    added picture to readme

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -2,10 +2,6 @@ import React from "react";
 
 import "./search.css";
 
-const button = () => {
-  console.log("lame");
-};
-
 const Search = () => {
   return (
     <div className="navbar-fixed">
@@ -13,9 +9,7 @@ const Search = () => {
         <div className="searchDiv">Image</div>
         <div className="searchDiv">Name</div>
         <div className="searchDiv">Species</div>
-        <div className="searchDiv" onClick={button}>
-          Gender
-        </div>
+        <div className="searchDiv">Gender</div>
         <div className="searchDiv">Location</div>
         <div className="searchDiv">Status</div>
       </nav>
